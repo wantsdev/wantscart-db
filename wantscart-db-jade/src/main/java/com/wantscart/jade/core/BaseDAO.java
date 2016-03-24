9caff4bf217930a33b7df6d8a5ca2b13c1a97cd1
张闯 <chuang.zhang@renren-inc.com>
2016-03-24 12:31:20 +0800

    fix bug

--- wantscart-db-jade/src/main/java/com/wantscart/jade/core/BaseDAO.java
+++ wantscart-db-jade/src/main/java/com/wantscart/jade/core/BaseDAO.java
@@ -21,7 +21,7 @@ public interface BaseDAO<T> {
     @SQL(value = "UPDATE " + TableSchema.TEMPLATE_TABLE + " SET " + TableSchema.TEMPLATE_COLUMN_PAIRS + " WHERE " + TableSchema.TEMPLATE_PK + " = :_id", type = SQLType.TEMPLATE)
     int update(@SQLParam("_id") Number id, @SQLParam("_t") T t);
 
-    @SQL(value = "DELETE " + TableSchema.TEMPLATE_TABLE + " WHERE " + TableSchema.TEMPLATE_PK + " = :_id", type = SQLType.TEMPLATE)
+    @SQL(value = "DELETE FROM " + TableSchema.TEMPLATE_TABLE + " WHERE " + TableSchema.TEMPLATE_PK + " = :_id", type = SQLType.TEMPLATE)
     int delete(@SQLParam("_id") Number id);
 
     @SQL(value = "SELECT " + TableSchema.TEMPLATE_PK + ", " + TableSchema.TEMPLATE_COLUMN_KEYS + " FROM " + TableSchema.TEMPLATE_TABLE + " WHERE " + TableSchema.TEMPLATE_PK + " = :_id", type = SQLType.TEMPLATE)
